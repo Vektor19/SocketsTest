8790fd2ad764644210c9d66fe07ee919a66552ee
Vektor19 <viktor.komissarov@knu.ua>
2025-02-19 22:03:36 +0200

    init projects

---
+++ Client/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "network.h"
+
+int main(int argc, char** argv)
+{
+	if (networking::Network::initialize())
+	{
+		std::cout << "Winsock api successfully initialized." << std::endl;
+	}
+	else
+	{
+		std::cerr << "Socket failed to create." << std::endl;
+	}
+	networking::Network::shutdown();
+	system("pause");
+	return 0;
+}
